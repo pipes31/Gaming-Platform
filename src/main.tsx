f378590d8fad3cbad26f9e60c37b6b3a6285e6b0
¨Felipe¨ <felipe961031@gmail.com>
2023-08-11 23:29:56 -0400

    Adding ColorMode Switcher

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider, ColorModeScript, color } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import theme from "./theme.ts";

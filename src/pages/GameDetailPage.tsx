1f4edc3b5de70d9bfc391b8ec84601b0881f9bc6
¨Felipe¨ <felipe961031@gmail.com>
2023-09-27 14:45:27 -0400

    setting ExpandableText Component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,8 @@
-import { Text, Heading, Spinner } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
+
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -9,10 +11,11 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text> DESCRIPTION : {game.description_raw} </Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };

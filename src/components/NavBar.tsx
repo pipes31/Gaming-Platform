85c8ac6fcb4f39a659f157707d279bf9ca06eb00
¨Felipe¨ <felipe961031@gmail.com>
2023-09-30 01:20:30 -0400

    refactor navbar & searchinput

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,22 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo-Pro.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image
+          src={logo}
+          boxSize="60px"
+          _hover={{
+            transform: "scale(1.1)",
+            transition: "transform 0.15s ease-in",
+          }}
+        />
+      </Link>
+
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
